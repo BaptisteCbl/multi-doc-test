4a6c736e730984fb43c0f25263934c459f5a02e3
Baptiste Combelles <baptiste.combelles@proton.me>
2023-07-05 10:34:34 +0200

    fc

---
+++ docs/make.jl
@@ -0,0 +1,82 @@
+using MultiDocumenter
+
+clonedir = mktempdir()
+
+docs = [
+    MultiDocumenter.DropdownNav(
+        "Debugging",
+        [
+            MultiDocumenter.MultiDocRef(
+                upstream = joinpath(clonedir, "Infiltrator"),
+                path = "inf",
+                name = "Infiltrator",
+                giturl = "https://github.com/JuliaDebug/Infiltrator.jl.git",
+            ),
+            MultiDocumenter.MultiDocRef(
+                upstream = joinpath(clonedir, "JuliaInterpreter"),
+                path = "debug",
+                name = "JuliaInterpreter",
+                giturl = "https://github.com/JuliaDebug/JuliaInterpreter.jl.git",
+            ),
+        ],
+    ),
+    MultiDocumenter.MegaDropdownNav(
+        "Mega Debugger",
+        [
+            MultiDocumenter.Column(
+                "Column 1",
+                [
+                    MultiDocumenter.MultiDocRef(
+                        upstream = joinpath(clonedir, "Infiltrator"),
+                        path = "inf",
+                        name = "Infiltrator",
+                        giturl = "https://github.com/JuliaDebug/Infiltrator.jl.git",
+                    ),
+                    MultiDocumenter.MultiDocRef(
+                        upstream = joinpath(clonedir, "JuliaInterpreter"),
+                        path = "debug",
+                        name = "JuliaInterpreter",
+                        giturl = "https://github.com/JuliaDebug/JuliaInterpreter.jl.git",
+                    ),
+                ],
+            ),
+            MultiDocumenter.Column(
+                "Column 2",
+                [
+                    MultiDocumenter.MultiDocRef(
+                        upstream = joinpath(clonedir, "Infiltrator"),
+                        path = "inf",
+                        name = "Infiltrator",
+                        giturl = "https://github.com/JuliaDebug/Infiltrator.jl.git",
+                    ),
+                    MultiDocumenter.MultiDocRef(
+                        upstream = joinpath(clonedir, "JuliaInterpreter"),
+                        path = "debug",
+                        name = "JuliaInterpreter",
+                        giturl = "https://github.com/JuliaDebug/JuliaInterpreter.jl.git",
+                    ),
+                ],
+            ),
+        ],
+    ),
+    MultiDocumenter.MultiDocRef(
+        upstream = joinpath(clonedir, "DataSets"),
+        path = "data",
+        name = "DataSets",
+        giturl = "https://github.com/JuliaComputing/DataSets.jl.git",
+        # or use ssh instead for private repos:
+        # giturl = "git@github.com:JuliaComputing/DataSets.jl.git",
+    ),
+]
+
+outpath = mkdir("docs/out")
+
+MultiDocumenter.make(
+    outpath,
+    docs;
+    search_engine = MultiDocumenter.SearchConfig(
+        index_versions = ["stable"],
+        engine = MultiDocumenter.FlexSearch,
+    ),
+    rootpath = "",
+)
