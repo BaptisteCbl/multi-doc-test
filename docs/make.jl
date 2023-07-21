29d3f4b104311946389fdb7c73526704e3f328fa
Baptiste Combelles <baptiste.combelles@proton.me>
2023-07-21 14:59:29 +0200

    foo

--- docs/make.jl
+++ docs/make.jl
@@ -85,33 +85,33 @@ MultiDocumenter.make(
     rootpath = "/MULTI-DOC-TEST/",
 )
 
-gitroot = normpath(joinpath(@__DIR__, ".."))
-run(`git pull`)
-outbranch = "gh-pages"
-has_outbranch = true
-if !success(`git checkout $outbranch`)
-    has_outbranch = false
-    if !success(`git switch --orphan $outbranch`)
-        @error "Cannot create new orphaned branch $outbranch."
-        exit(1)
-    end
-end
-for file in readdir(gitroot; join = true)
-    endswith(file, ".git") && continue
-    rm(file; force = true, recursive = true)
-end
-for file in readdir(outpath)
-    cp(joinpath(outpath, file), joinpath(gitroot, file))
-end
-run(`git add .`)
-if success(`git commit -m 'Aggregate documentation'`)
-    @info "Pushing updated documentation."
-    if has_outbranch
-        run(`git push`)
-    else
-        run(`git push -u origin $outbranch`)
-    end
-    run(`git checkout main`)
-else
-    @info "No changes to aggregated documentation."
-end
+# gitroot = normpath(joinpath(@__DIR__, ".."))
+# run(`git pull`)
+# outbranch = "gh-pages"
+# has_outbranch = true
+# if !success(`git checkout $outbranch`)
+#     has_outbranch = false
+#     if !success(`git switch --orphan $outbranch`)
+#         @error "Cannot create new orphaned branch $outbranch."
+#         exit(1)
+#     end
+# end
+# for file in readdir(gitroot; join = true)
+#     endswith(file, ".git") && continue
+#     rm(file; force = true, recursive = true)
+# end
+# for file in readdir(outpath)
+#     cp(joinpath(outpath, file), joinpath(gitroot, file))
+# end
+# run(`git add .`)
+# if success(`git commit -m 'Aggregate documentation'`)
+#     @info "Pushing updated documentation."
+#     if has_outbranch
+#         run(`git push`)
+#     else
+#         run(`git push -u origin $outbranch`)
+#     end
+#     run(`git checkout main`)
+# else
+#     @info "No changes to aggregated documentation."
+# end
